e86b65fa45334421b7dc8617ace8656a53a4d815
Karl Ostmo <kostmo@gmail.com>
2011-07-11 01:54:37 -0700

    Improved icon drawing

--- graphics/draw_icon_variants.py
+++ graphics/draw_icon_variants.py
@@ -1,97 +1,100 @@
 #!/usr/bin/env python
 
+from math import sqrt
 import cairo
+
 image_side_pixel_count = 19
+output_dir = "../extension/icons"
 
 def backward_arrow(ctx):
 	ctx.set_source_rgb(0.9, 0.5, 0.2) # Solid color
 	ctx.set_line_width(0.2)
 
 	ctx.save()
-	ctx.translate(0.1, 0.1)
-	ctx.translate(0.5, 0)
-	ctx.translate(0, 0.4)
-	ctx.scale(-0.5, 0.4)
-	draw_triangle(ctx)
+	ctx.translate(0.6, 0.1)
+	ctx.scale(0.8, 0.8)	# With 0.1 margins, we must subtract a total of 0.2 from the side length of the unit square
+
+	ctx.translate(0, 0.5)
+
+	ctx.scale(0.5, 0.5)
+
+	ctx.translate(1/sqrt(2), 0)
+	ctx.scale(-1, 1)
+
+	unit_equilateral_triangle(ctx)
 	ctx.restore()
 
 def forward_arrow(ctx):
+	# TODO Use a decorator to scale and translate appropriately
 	ctx.set_line_width(0.2)
 	ctx.set_source_rgb(0.2, 0.8, 0.5) # Solid color
 
 	ctx.save()
-	ctx.translate(0.1, 0.1)
-	ctx.scale(0.5, 0.4)
-	draw_triangle(ctx)
+	ctx.translate(0.6, 0.1)
+
+	ctx.scale(0.8, 0.8)
+
+	ctx.scale(0.5, 0.5)
+	unit_equilateral_triangle(ctx)
 	ctx.restore()
 
-def draw_triangle(ctx):
-
+def unit_equilateral_triangle(ctx):
+	'''Draw an right-pointing equilateral triangle in the unit square against the left edge.'''
 	ctx.set_line_join(cairo.LINE_JOIN_ROUND)
-	ctx.move_to(1, 0.5)
+	ctx.move_to(1/sqrt(2), 0.5)
 	ctx.line_to (0, 0)
 	ctx.line_to (0, 1)
 	ctx.close_path ()
 	ctx.stroke()
 
-def plus_sign(ctx):
-
-	ctx.save()
-	ctx.translate(0.7, 0.7)
-	ctx.scale(0.3, 0.3)
-	ctx.set_source_rgb(0, 0, 0) # Solid color
+def unit_plus_sign(ctx):
+	'''Draw a plus sign filling the unit square.'''
 	ctx.rectangle(1/3.0, 0, 1/3.0, 1)
 	ctx.rectangle(0, 1/3.0, 1, 1/3.0)
 	ctx.fill()
+
+def plus_sign(ctx):
+	ctx.save()
+	ctx.translate(0.1, 0.1)
+	ctx.scale(0.3, 0.3)
+	ctx.set_source_rgb(0, 0, 0) # Solid color
+	unit_plus_sign(ctx)
 	ctx.restore()
 
 keywords = {
+	"manual": plus_sign,
 	"prev": backward_arrow,
 	"next": forward_arrow,
-	"manual": plus_sign
 }
+sorted_keywords = sorted(keywords.keys())
 
 def render_combination(ctx, combination):
 	for element in combination:
 		keywords[element](ctx)
 
-def generate_combination(i, j, k):
-	combination = []
-	if i:
-		combination.append("manual")
-	if j:
-		combination.append("prev")
-	if k:
-		combination.append("next")
-	combination.sort()
-	return combination
+def generate_combination( bits ):
+	return [keyword for i, keyword in enumerate(sorted_keywords) if (bits >> i) & 1]
+
+def write_combo_image(combo):
+
+	import os
+	filename_base = os.path.join(output_dir, "_".join(combo))
+	filename = filename_base + ".png"
+
+	surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, image_side_pixel_count, image_side_pixel_count)
+	ctx = cairo.Context(surface)
+	ctx.scale(image_side_pixel_count, image_side_pixel_count)
+
+	render_combination(ctx, combo)
+
+	print "Writing png to path:", filename
+	surface.write_to_png( filename )
 
 if __name__ == "__main__":
 
-	import os
-
-	output_dir = "../extension/icons"
-	'''
-	if not os.path.exists(output_dir):
-		os.mkdir(output_dir)
-	'''
-	combinations = []
-	for i in range(2):
-		for j in range(2):
-			for k in range(2):
-				if j or k:
-					combinations.append(generate_combination(i, j, k))
-
+	combinations = [generate_combination( i ) for i in range(2**len(keywords)) if i]
+	print "Combinations:", combinations
 
 	for combo in combinations:
-		filename_base = os.path.join(output_dir, "_".join(combo))
-
-		surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, image_side_pixel_count, image_side_pixel_count)
-		ctx = cairo.Context(surface)
-		ctx.scale(image_side_pixel_count, image_side_pixel_count)
-
-		render_combination(ctx, combo)
-
-		filename = filename_base + ".png"
-		surface.write_to_png( filename )
+		write_combo_image(combo)
 
